a7b0e924c9110cfa8b658fa9748dc7847399858e
Chris Harding <wablahflababablah@gmail.com>
2015-10-06 17:37:46 -0400

    Updated config loading to match #1
    Some minor differences from #1:
      - 'name' was dropped from the item config files as it is inferred
      - 'srcs' array was replaced with the string 'main'
    
    To load the module 'foo' the relevant paths are searched for directories
    of the given name that contain a 'config.json'. This config file is used
    as the config file for the module or agent (at a class level).

--- halibot.py
+++ halibot.py
@@ -6,6 +6,7 @@ import json
 import threading
 import imp
 import inspect
+import os
 from halmodule import HalModule
 from halagent import HalAgent
 import sys
@@ -13,11 +14,55 @@ sys.path.append(".")
 #from asyncio import Queue
 from queue import Queue
 
+# This will find a path to a module/agent
+#  name - the name of the module/agent
+#  paths - the path list to search
+def find_basepath(name, paths):
+	for path in paths:
+		found = path + os.sep + name + os.sep
+		config = found + "config.json"
+
+		if os.path.isfile(config):
+			return found
+
+	raise Exception("Could not find module/agent '" + name + "'!");
+
+# Loads a module or agent class
+#  name - the name of the item to load
+#  paths - the paths to search for the item to load
+#  superclass - the superclass required for things to subclass
+#  dest - the dict to add the class object to
+def load_item(name, paths, superclass, dest):
+	basepath = find_basepath(name, paths)
+	supername = superclass.__name__
+
+	with open(basepath + "config.json", "r") as f:
+		# TODO something more with this
+		config = json.loads(f.read())
+
+	src = basepath + config["main"]
+
+	try:
+		pymod = imp.load_source(name, src)
+	except Exception as e:
+		print(e)
+		return False
+
+	for objname, obj in inspect.getmembers(pymod):
+		if inspect.isclass(obj) and issubclass(obj, superclass) and objname != supername:
+			# TODO handle multiple superclasses per python module somehow?
+			dest[name] = obj
+			return
+
+	raise Exception("Could not find a " + supername + " in '" + src + "'!")
+
 class Halibot():
 
 	config = {}
 	agents = {}
-	modules = []
+	modules = {}
+	agent_classes = {}
+	module_classes = {}
 	queue = None
 	
 	def __init__(self):
@@ -26,10 +71,9 @@ class Halibot():
 	# Start the Hal instance
 	def start(self, block=True):
 
-		# TODO: Make these do things
 		self._load_config()
-		self._load_agents()
-		self._load_modules()
+		self._instantiate_agents()
+		self._instantiate_modules()
 
 		self._start_route()
 
@@ -37,48 +81,54 @@ class Halibot():
 		with open("config.json","r") as f:
 			self.config = json.loads(f.read())
 
-	# TODO: Make robust
-	def _load_agents(self):
-		for a in self.config["agents"]:
-			self._load_agent(a)
+			self.module_path = self.config["module-path"]
+			self.agent_path = self.config["agent-path"]
 
-	# TODO: Make robust
-	def _load_modules(self):
-		for m in self.config["modules"]:
-			self._load_module(m)
+	def _instantiate_agents(self):
+		inst = self.config["agent-instances"]
 
-	def _load_agent(self, conf):
-		f, p, d = imp.find_module(conf["path"][:-3])
-		inst = conf.get("name")
+		for k in inst.keys():
+			# TODO include directive
 
-		try:
-			agt = imp.load_module(conf["path"][:-3], f, p, d)
-		except Exception as e:
-			print(e)
-			return False
+			conf = inst[k]
+			conf["name"] = k
+			of = conf["of"]
 
-		for name, obj in inspect.getmembers(agt):
-			if inspect.isclass(obj) and issubclass(obj, HalAgent) and name != "HalAgent":
-				if not inst:
-					inst = name
+			if not of in self.agent_classes:
+				self._load_agent(of)
 
-				self.agents[inst] = obj(self, conf)
-				self.agents[inst].init()
+			obj = self.agent_classes[of]
+			self.agents[k] = obj(self, conf)
+			self.agents[k].init()
+			print("Instantiated agent '" + k + "'")
 
-	def _load_module(self, conf):
-		f, p, d = imp.find_module(conf["path"][:-3])
-		inst = conf.get("name")
+	def _instantiate_modules(self):
+		inst = self.config["module-instances"]
 
-		try:
-			mod = imp.load_module(conf["path"][:-3], f, p, d)
-		except Exception as e:
-			print(e)
-			return False
+		for k in inst.keys():
+			# TODO include directive
 
-		for name, obj in inspect.getmembers(mod):
-			if inspect.isclass(obj) and issubclass(obj, HalModule) and name != "HalModule":
-				self.modules.append(obj(self, conf))
-				self.modules[-1].init()
+			conf = inst[k]
+			conf["name"] = k
+			of = conf["of"]
+
+			if not of in self.module_classes:
+				self._load_module(of)
+
+			obj = self.module_classes[of]
+			self.modules[k] = obj(self, conf)
+			self.modules[k].init()
+			print("Instantiated module '" + k + "'")
+
+
+	def _load_agent(self, name):
+		load_item(name, self.agent_path, HalAgent, self.agent_classes)
+		print("Loaded agent class '" + name + "'")
+
+	def _load_module(self, name):
+		load_item(name, self.module_path, HalModule, self.module_classes)
+		print("Loaded module class '" + name + "'")
+
 
 	def _start_route(self):
 		t = threading.Thread(target=self._do_route)
@@ -92,7 +142,7 @@ class Halibot():
 			if m["source"] == "module":
 				self.agents[m["context"]["agent"]].receive(m)
 			elif m["source"] == "agent":
-				for a in self.modules:
+				for a in self.modules.values():
 					a.receive(m)
 
 	def receive(self, msg):
