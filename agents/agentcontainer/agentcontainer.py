60a3ae95d3181d4242e35d75ece8e955b8f1a179
Chris Harding <wablahflababablah@gmail.com>
2016-09-07 23:34:17 +0000

    Update containers for RI support

--- agents/agentcontainer/agentcontainer.py
+++ agents/agentcontainer/agentcontainer.py
@@ -2,6 +2,8 @@ from halibot import HalAgent
 
 class AgentContainer(HalAgent):
 
-	def receive(self, msg):
-		self.send_to(msg, self.config.get('contents', []))
+	def receive(self, msg, whom):
+		suff = '/' + whom if whom != '' else ''
+		cont = self.config.get('contents', [])
+		self.send_to(msg, [ name + suff for name in cont ])
 
