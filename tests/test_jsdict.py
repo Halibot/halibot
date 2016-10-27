d9212a731820814b6dd1dea783be36a2168ddf0d
Chris Harding <wablahflababablah@gmail.com>
2016-10-27 04:33:20 +0000

    Test jsdict

---
+++ tests/test_jsdict.py
@@ -0,0 +1,20 @@
+
+import unittest
+from halibot.jsdict import jsdict
+
+class TestJsdict(unittest.TestCase):
+
+    def test_jsdict(self):
+        d = jsdict()
+
+        d.foo = 'iamblichus'
+        self.assertEqual(d['foo'], 'iamblichus')
+        self.assertEqual(d.foo, 'iamblichus')
+
+        d['bar'] = 'porphyry'
+        self.assertEqual(d['bar'], 'porphyry')
+        self.assertEqual(d.bar, 'porphyry')
+
+if __name__ == '__main__':
+	unittest.main()
+
