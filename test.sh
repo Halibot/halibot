afdd37c17f399f279cf376e2f954fff93e4cb480
Chris Harding <wablahflababablah@gmail.com>
2016-10-27 03:53:44 +0000

    Append coverage reports

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ set -e
 
 command -v python 2>&1 >/dev/null && { PYTHON=python; }
 command -v python3 2>&1 >/dev/null && { PYTHON=python3; }
-command -v coverage 2>&1 >/dev/null && { PYTHON="coverage run"; }
+command -v coverage 2>&1 >/dev/null && { coverage erase; PYTHON="coverage run -a"; }
 
 for TEST in tests/test_*.py
 do
