6dedad27332170d705ab89aa503e460629220977
Chris Harding <wablahflababablah@gmail.com>
2016-10-27 04:34:16 +0000

    More thorough message sending testing
    Now test sending by default to all modules; test sending to a connected
    proper subset; test sending to a connect subset which is all modules;
    test that the whom is propogated correctly

--- tests/test_core.py
+++ tests/test_core.py
@@ -7,10 +7,10 @@ import unittest
 
 class StubModule(halibot.HalModule):
 	inited = False
-	received = []
 
 	def init(self):
 		self.inited = True
+		self.received = []
 
 	def receive(self, msg):
 		self.received.append(msg)
@@ -40,35 +40,65 @@ class TestCore(util.HalibotTestCase):
 	def test_send_recv(self):
 		agent = StubAgent(self.bot)
 		mod = StubModule(self.bot)
+		mod2 = StubModule(self.bot)
 		self.bot.add_instance('stub_agent', agent)
 		self.bot.add_instance('stub_mod', mod)
+		self.bot.add_instance('stub_mod2', mod2)
 
+		# mod should receive: foo, bar, baz, qua
+		# mod2 should receive: foo, bar, qua
 		foo = halibot.Message(body='foo')
 		bar = halibot.Message(body='bar')
 		baz = halibot.Message(body='baz', origin='glub_agent')
+		qua = halibot.Message(body='qua')
 
+		agent.dispatch(foo) # 0
+		agent.send_to(bar, [ 'stub_mod/able', 'stub_mod2/baker' ] ) # 1
 		agent.connect(mod)
-		agent.dispatch(foo)
-		agent.send_to(bar, [ 'stub_mod' ])
-		agent.dispatch(baz)
+		agent.dispatch(baz) # 2
+		agent.connect(mod2)
+		agent.dispatch(qua) # 3
 
 		# TODO do something sensible here
 		timeout = 10
 		increment = .1
-		while timeout > 0 and len(mod.received) != 3:
+		while timeout > 0 and len(mod.received) != 4 and len(mod2.received) != 3:
 			time.sleep(increment)
 			timeout -= increment
 
-		self.assertEqual(3, len(mod.received))
+		# Check mod received mesages
+		self.assertEqual(4, len(mod.received))
 		self.assertEqual(foo.body, mod.received[0].body)
 		self.assertEqual(bar.body, mod.received[1].body)
 		self.assertEqual(baz.body, mod.received[2].body)
+		self.assertEqual(qua.body, mod.received[3].body)
+		self.assertEqual('', mod.received[0].whom())
+		self.assertEqual('able', mod.received[1].whom())
+		self.assertEqual('', mod.received[2].whom())
+		self.assertEqual('', mod.received[3].whom())
 		self.assertEqual('stub_mod', mod.received[0].target)
-		self.assertEqual('stub_mod', mod.received[1].target)
+		self.assertEqual('stub_mod/able', mod.received[1].target)
 		self.assertEqual('stub_mod', mod.received[2].target)
+		self.assertEqual('stub_mod', mod.received[3].target)
 		self.assertEqual('stub_agent', mod.received[0].origin)
 		self.assertEqual('stub_agent', mod.received[1].origin)
 		self.assertEqual('glub_agent', mod.received[2].origin)
+		self.assertEqual('stub_agent', mod.received[3].origin)
+
+		# Check mod2 received mesages
+		self.assertEqual(3, len(mod2.received))
+		self.assertEqual(foo.body, mod2.received[0].body)
+		self.assertEqual(bar.body, mod2.received[1].body)
+		self.assertEqual(qua.body, mod2.received[2].body)
+		self.assertEqual('', mod.received[0].whom())
+		self.assertEqual('able', mod.received[1].whom())
+		self.assertEqual('', mod.received[2].whom())
+		self.assertEqual('stub_mod2', mod2.received[0].target)
+		self.assertEqual('stub_mod2/baker', mod2.received[1].target)
+		self.assertEqual('stub_mod2', mod2.received[2].target)
+		self.assertEqual('stub_agent', mod2.received[0].origin)
+		self.assertEqual('stub_agent', mod2.received[1].origin)
+		self.assertEqual('stub_agent', mod2.received[2].origin)
 
 if __name__ == '__main__':
 	unittest.main()
