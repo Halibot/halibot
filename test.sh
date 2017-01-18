5cba44eb0ff117160576a6380d74ba6e540910c9
Chris Harding <wablahflababablah@gmail.com>
2017-01-18 00:58:00 +0000

    Test with specific python versions

--- test.sh
+++ test.sh
@@ -4,6 +4,9 @@ set -e
 
 command -v python 2>&1 >/dev/null && { PYTHON=python; }
 command -v python3 2>&1 >/dev/null && { PYTHON=python3; }
+command -v python3.4 2>&1 >/dev/null && { PYTHON=python3.4; }
+command -v python3.5 2>&1 >/dev/null && { PYTHON=python3.5; }
+command -v python3.6 2>&1 >/dev/null && { PYTHON=python3.6; }
 command -v coverage 2>&1 >/dev/null && { coverage erase; PYTHON="coverage run -a"; }
 
 for TEST in tests/test_*.py
