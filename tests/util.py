31712eb7fddaad627a29704f881bd72c44c82ce5
Eric Richter <richteer@lastprime.net>
2017-09-14 12:44:39 -0500

    tests: set logging level to 'critical' for tests
    Tests for the most part shouldn't be relying on logged messages to
    determine success or failure, therefore we should safely be able to
    silence them by setting the level to 'critical'.

--- tests/util.py
+++ tests/util.py
@@ -2,12 +2,15 @@
 
 import halibot
 import unittest
+import logging
 
 # Provides a unique bot in self.bot for every test case
 class HalibotTestCase(unittest.TestCase):
 
 	def setUp(self):
+		# Silence expected error messages
+		logging.basicConfig(level=logging.CRITICAL)
 		self.bot = halibot.Halibot(use_config=False)
-	
+
 	def tearDown(self):
 		self.bot.shutdown()
