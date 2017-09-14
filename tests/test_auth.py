69afb716f30173c95a4d6ce970b047c862a31bd5
Eric Richter <richteer@lastprime.net>
2017-09-14 12:46:43 -0500

    tests: add more tests for the auth system
    This commit adds tests for:
     - grantPermission
     - revokePermission
     - load_perms
     - write_perms

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -4,6 +4,7 @@ import time
 import util
 import halibot
 import unittest
+import os
 
 class StubModuleFunc(halibot.HalModule):
 
@@ -21,8 +22,8 @@ class StubModuleDec(halibot.HalModule):
 
 	def init(self):
 		self.called = False
-	
-	@halibot.halauth.hasPermission("Foo")
+
+	@halibot.halauth.hasPermission("Foo", reply=True)
 	def function(self, msg):
 		self.called = True
 
@@ -30,6 +31,38 @@ class StubModuleDec(halibot.HalModule):
 		self.function(msg)
 
 class TestAuth(util.HalibotTestCase):
+
+	def test_grantperm(self):
+		self.bot.auth.perms = []
+		self.bot.auth.enabled = False
+		self.bot.auth.grantPermission("foo", "bar", "baz")
+
+		self.assertEqual(len(self.bot.auth.perms), 0)
+
+		self.bot.auth.enabled = True
+
+		self.bot.auth.grantPermission("foo", "bar", "baz")
+		self.assertEqual(len(self.bot.auth.perms), 1)
+		self.assertEqual(self.bot.auth.perms[0][0], "foo")
+		self.assertEqual(self.bot.auth.perms[0][1], "bar")
+		self.assertEqual(self.bot.auth.perms[0][2], "baz")
+
+	def test_revokeperm(self):
+		self.bot.auth.perms = [("foo", "bar", "baz")]
+		self.bot.auth.enabled = False
+
+		self.bot.auth.revokePermission("foo", "bar", "baz")
+		# Permissions aren't enabled, so we should ignore revocations
+		self.assertEqual(len(self.bot.auth.perms), 1)
+
+		self.bot.auth.enabled = True
+
+		self.bot.auth.revokePermission("foo", "bar", "baz")
+		self.assertEqual(len(self.bot.auth.perms), 0)
+
+		# This should remain empty, and fail to find the perm to revoke
+		self.bot.auth.revokePermission("foo", "bar", "baz")
+		self.assertEqual(len(self.bot.auth.perms), 0)
 
 	def test_hasperm_func(self):
 		self.bot.auth.enabled = True
@@ -53,6 +86,14 @@ class TestAuth(util.HalibotTestCase):
 		stub.receive(msg)
 		self.assertFalse(stub.called)
 
+		# NOTE: This assumes that enabled = allow all perms.
+		# Is this really expected behavior?
+		stub.called = False
+		self.bot.auth.enabled = False
+		self.bot.auth.revokePermission(ri, user, perm)
+		stub.receive(msg)
+		self.assertTrue(stub.called)
+
 	def test_hasperm_dec(self):
 		self.bot.auth.enabled = True
 		stub = StubModuleDec(self.bot)
@@ -75,8 +116,37 @@ class TestAuth(util.HalibotTestCase):
 		stub.receive(msg)
 		self.assertFalse(stub.called)
 
+	def test_load_perms(self):
+		with open("testperms.json", "w") as f:
+			f.write("[]")
+
+		self.bot.auth.load_perms("testperms.json")
+		self.assertEqual(len(self.bot.auth.perms), 0)
+
+		os.remove("testperms.json")
+		self.bot.auth.enabled = False
+		self.bot.auth.load_perms("testperms.json")
+		self.assertTrue(self.bot.auth.enabled)
+
+	def test_write_perms(self):
+		self.bot.auth.enabled = True
+		self.bot.auth.path = "testperms.json"
+		self.bot.auth.perms = [("foo", "bar", "baz")]
+		self.bot.auth.write_perms()
+
+		self.bot.auth.enabled = False
+		self.bot.auth.perms = []
+
+		self.bot.auth.load_perms("testperms.json")
+		self.assertTrue(self.bot.auth.enabled)
+		p = self.bot.auth.perms[0]
+		self.assertEqual(p[0], "foo")
+		self.assertEqual(p[1], "bar")
+		self.assertEqual(p[2], "baz")
+
+		os.remove("testperms.json")
+
 
 
 if __name__ == '__main__':
 	unittest.main()
-
