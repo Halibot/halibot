22622d4c1ded224b152fd6ed47d91369d7cc3119
Eric Richter <richteer@lastprime.net>
2017-09-19 18:39:41 -0500

    halibot: introduce CommandModule subclass of HalModule
    Initial implementation of the CommandModule class of HalModule.
    
    To implement a CommandModule:
     - Inherit from the halibot.CommandModule class
     - Define a class variable that is a
        dict: "command" -> func(self, string, msg=msg)
    
    Example:
    
    class Foo(CommandModule):
    	def init(self):
    		self.commands = {"hello", self.hello_}
    
    	def hello_(self, string, msg=None):
    		return self.reply(msg, body="Hello World!")
    
    Note: it is recommended practice to trail command methods with an underscore
    so as not to accidentally override internal halobject methods.
    
    some fixes

--- halibot/__init__.py
+++ halibot/__init__.py
@@ -3,3 +3,4 @@ from .halagent import HalAgent
 from .halmodule import HalModule
 from .halconfigurer import HalConfigurer
 from .message import Message
+from .commandmodule import CommandModule, AsArgs
