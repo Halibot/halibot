6e14ac8a83b4c18975eeb14e08e9c9b02322f80a
Eric Richter <richteer@lastprime.net>
2017-10-21 11:57:54 -0500

    CLEAN: spaces instead of tabs. why.

--- tests/test_jsdict.py
+++ tests/test_jsdict.py
@@ -4,17 +4,16 @@ from halibot.jsdict import jsdict
 
 class TestJsdict(unittest.TestCase):
 
-    def test_jsdict(self):
-        d = jsdict()
+	def test_jsdict(self):
+		d = jsdict()
 
-        d.foo = 'iamblichus'
-        self.assertEqual(d['foo'], 'iamblichus')
-        self.assertEqual(d.foo, 'iamblichus')
+		d.foo = 'iamblichus'
+		self.assertEqual(d['foo'], 'iamblichus')
+		self.assertEqual(d.foo, 'iamblichus')
 
-        d['bar'] = 'porphyry'
-        self.assertEqual(d['bar'], 'porphyry')
-        self.assertEqual(d.bar, 'porphyry')
+		d['bar'] = 'porphyry'
+		self.assertEqual(d['bar'], 'porphyry')
+		self.assertEqual(d.bar, 'porphyry')
 
 if __name__ == '__main__':
 	unittest.main()
-
