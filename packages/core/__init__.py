ca24d8104f338e4659997fdb28d7470547d9aed3
Eric Richter <richteer@lastprime.net>
2022-09-05 13:18:25 -0500

    packages/core: add new core admin module
    This may be used in the future to store useful commands for administrative
    purposes. Also demonstrates use of permissions -- either this module or
    another should provide the ability to modify permissions without needing
    to edit the json file manually

--- packages/core/__init__.py
+++ packages/core/__init__.py
@@ -1,2 +1,3 @@
 
 from .help import Help
+from .admin import Admin
---
+++ packages/core/admin.py
@@ -0,0 +1,30 @@
+#
+# Admin control module
+#  Basic commands to manage a running halibot instance from an agent
+#
+from halibot import CommandModule, Message
+from halibot.halauth import hasPermission
+
+class Admin(CommandModule):
+
+	VERSION = "1.0.0"
+	HAL_MINIMUM = "0.1"
+
+	def init(self):
+		self.commands = {
+			"shutdown": self.cmd_shutdown,
+			"reload": self.cmd_reload,
+		}
+
+	@hasPermission("ADMIN", reply=True)
+	def cmd_shutdown(self, args, msg=None):
+		self._hal.shutdown()
+
+	@hasPermission("ADMIN", reply=True)
+	def cmd_reload(self, args, msg=None):
+		try:
+			self._hal.reload(args)
+		except Exception as e:
+			self.reply(msg, body=f"Could not reload '{args}': {e}")
+		else:
+			self.reply(msg, body=f"Successfully reloaded '{args}'!")
